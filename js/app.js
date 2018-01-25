5635fe8cd924da0419bd2bd261ea6711bc6dca10
dayamll <dayamllerenacastro@gmail.com>
2018-01-25 18:22:37 -0500

    cambiando variables

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-let start = prompt('Enter your Message!!!');
+const start = prompt('Enter your Message!!!');
 // Creando funcion para el cifrado del mensaje por medio del codigo cesar!
 function cipher(message) {
   let cipherMessage = '';
-  let n = 33;
+  const n = 33;
   for (let i = 0; i < message.length; i++) {
-    let x = message[i].charCodeAt(0);
-    let asciiCode = (x - 65 + n) % 26 + 65;
+    const x = message[i].charCodeAt(0);
+    const asciiCode = (x - 65 + n) % 26 + 65;
     cipherMessage += String.fromCharCode(asciiCode);
   }
   return cipherMessage;
@@ -13,12 +13,12 @@ function cipher(message) {
 // Creando segunnda funcion para el decifrado del mensaje por medio del codigo cesar!
 function decipher(cipherMessage) {
   let message = '';
-  let n = 33;
+  const n = 33;
   for (let i = 0; i < cipherMessage.length; i++) {
-    let x = cipherMessage[i].charCodeAt(0);
-    let asciiCode = (x - 65 + 26 + 26 - n) % 26 + 65;
+    const x = cipherMessage[i].charCodeAt(0);
+    const asciiCode = (x - 65 + 26 + 26 - n) % 26 + 65;
     message += String.fromCharCode(asciiCode);
   }
   return message;
 }
-console.log('This is your result!!' + ' ' + cipher(start));
+console.log(`This is your result!! ${cipher(start)}`);
