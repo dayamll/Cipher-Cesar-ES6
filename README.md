107e996726d4788cc65559bb24a1825f5cedcc65
dayamll <dayamllerenacastro@gmail.com>
2018-01-26 02:14:14 -0500

    subiendo funciones,estilos y README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cifrado y Descifrado Cesar ES6
+_________________________________________________
+
+## Convirtiendo javascript 5 a ES6...
+
+* En la siguiente pagina web el usuario podra escribir texto en los inputs, y estos regresaran el texto convertido en codigo ascii.
+
+
+## **Version Mobile:**
+
+
+![](assets/images/mobile.png)
+
+## **Version Desktop:**
+
+![](assets/images/desktop.png)
--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
-const start = prompt('Enter your Message!!!');
-// Creando funcion para el cifrado del mensaje por medio del codigo cesar!
-function cipher(message) {
+// Declaracion de variables ...
+
+let btnCipher = document.getElementById('cipherMessage');
+let btnDescipher = document.getElementById('decipherMessage');
+let inputMessage = document.getElementById('inputMessage');
+let inputMessage2 = document.getElementById('inputMessage2');
+let showMessage = document.getElementById('showCipher');
+
+// // const start = prompt('Enter your Message!!!');
+// // Creando funcion para el cifrado del mensaje por medio del codigo ascii!
+btnCipher.addEventListener('click', cipher);
+
+function cipher() {
   let cipherMessage = '';
-  const n = 33;
-  for (let i = 0; i < message.length; i++) {
-    const x = message[i].charCodeAt(0);
-    const asciiCode = (x - 65 + n) % 26 + 65;
-    cipherMessage += String.fromCharCode(asciiCode);
+  let message = inputMessage.value;
+  const number = 33;
+  for (i = 0; i < message.length; i++) {
+    let ascii = message[i].charCodeAt(0);
+    if (ascii >= 65 && ascii <= 90) // Mayusculas A = 65  Z = 90
+      cipherMessage += String.fromCharCode((ascii - 65 + number) % 26 + 65); 
+    else if (ascii >= 97 && ascii <= 122)// Minusculas a = 97  z = 122
+      cipherMessage += String.fromCharCode((ascii - 97 + number) % 26 + 97); 
   }
-  return cipherMessage;
+  showMessage.innerHTML = `<p> Ciphered word: ${cipherMessage} </p>`;
 }
-// Creando segunnda funcion para el decifrado del mensaje por medio del codigo cesar!
-function decipher(cipherMessage) {
-  let message = '';
-  const n = 33;
-  for (let i = 0; i < cipherMessage.length; i++) {
-    const x = cipherMessage[i].charCodeAt(0);
-    const asciiCode = (x - 65 + 26 + 26 - n) % 26 + 65;
-    message += String.fromCharCode(asciiCode);
+
+// // Creando segunnda funcion para el decifrado del mensaje por medio del codigo ascii!
+btnDescipher.addEventListener('click', descipher);
+
+function descipher() {
+  let decipherMessage = '';
+  let message2 = inputMessage2.value;
+  for (i = 0; i < message2.length; i++) {
+    let ascii = message2[i].charCodeAt(0);
+    const number = 33;
+    if (ascii >= 65 && ascii <= 90) // Mayusculas A = 65  Z = 90
+      decipherMessage += String.fromCharCode((ascii - 65 - number + 26 * 2) % 26 + 65);
+    else if (ascii >= 97 && ascii <= 122) // Minusculas a = 97  z = 122
+      decipherMessage += String.fromCharCode((ascii - 97 - number + 26 * 2) % 26 + 97);
   }
-  return message;
+  showMessage.innerHTML = `<p> Deciphered word: ${decipherMessage} </p>`;
 }
-console.log(`This is your result!! ${cipher(start)}`);
+
+// Funcion para limpiar el historial del texto ingresado!!
+
+let clearText = () => {
+  inputMessage.value = '';
+  inputMessage2.value = '';
+  showMessage.textContent = '';
+};
+clear.addEventListener('click', clearText);
